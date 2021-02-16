34ec7ddf66fede7d14ea4de93f3306e6c359139b
Gabriel <knuckles4x@gmail.com>
2021-02-15 23:08:17 -0300

    primeiro component feito na mao

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Meu título massa';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
