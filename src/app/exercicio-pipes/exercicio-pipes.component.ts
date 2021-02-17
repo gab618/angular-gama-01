2661526c9598b894d6eaf676a687d1d5d15c590e
Gabriel <knuckles4x@gmail.com>
2021-02-17 20:15:34 -0300

    exercicio pipes

--- src/app/exercicio-pipes/exercicio-pipes.component.ts
+++ src/app/exercicio-pipes/exercicio-pipes.component.ts
@@ -12,4 +12,21 @@ export class ExercicioPipesComponent {
     precoAluguel: 15.45,
     dataLancamento: new Date(2019, 11, 25),
   };
+
+  compras = [
+    {
+      produto: 'lâmpadas',
+      valor: 299.99,
+      quantidade: 2,
+      peso: 0,
+      data: new Date(2020, 1, 1, 15, 20),
+    },
+    {
+      produto: 'farinha',
+      valor: 450.29,
+      quantidade: 2,
+      peso: 29.33333333,
+      data: new Date(2020, 1, 10, 19, 30),
+    },
+  ];
 }
