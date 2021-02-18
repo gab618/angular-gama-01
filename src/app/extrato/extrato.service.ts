a273e91a6ebcbb46e0691ad951071331926e0466
Gabriel <knuckles4x@gmail.com>
2021-02-18 18:17:27 -0300

    using env vars

--- src/app/extrato/extrato.service.ts
+++ src/app/extrato/extrato.service.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Transacao } from './extrato.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExtratoService {
+  API_URL = environment.API_URL;
   constructor(private http: HttpClient) {}
 
   getTransacoes(): Observable<Transacao[]> {
-    return this.http.get<Transacao[]>(
-      'http://my-json-server.typicode.com/vitorfgsantos/fake-api/transacoes'
-    );
+    return this.http.get<Transacao[]>(`${this.API_URL}/transacoes`);
   }
 }
