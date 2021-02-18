b2e54ab71bda59cede262c858850a8e916bd72f1
Gabriel <knuckles4x@gmail.com>
2021-02-18 16:20:14 -0300

    create my first service

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ExtratoService } from './extrato.service';
 
 @Component({
   selector: 'app-extrato',
   templateUrl: './extrato.component.html',
-  styleUrls: ['./extrato.component.scss']
+  styleUrls: ['./extrato.component.scss'],
 })
 export class ExtratoComponent implements OnInit {
+  transacoes = [];
 
-  constructor() { }
+  constructor(private extratoService: ExtratoService) {}
 
   ngOnInit(): void {
+    this.transacoes = this.extratoService.getTransacoes();
   }
-
 }
