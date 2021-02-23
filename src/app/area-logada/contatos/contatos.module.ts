348a360b5160b50ff4f6cd72e529eaf6450db3ab
Gabriel <knuckles4x@gmail.com>
2021-02-23 17:06:36 -0300

    data driven form

--- src/app/area-logada/contatos/contatos.module.ts
+++ src/app/area-logada/contatos/contatos.module.ts
@@ -5,9 +5,14 @@ import { ContatosRoutingModule } from './contatos-routing.module';
 import { ContatosComponent } from './contatos.component';
 import { DetalhesContatoComponent } from './detalhes-contato/detalhes-contato.component';
 import { NovoContatoComponent } from './novo-contato/novo-contato.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ContatosComponent, DetalhesContatoComponent, NovoContatoComponent],
-  imports: [CommonModule, ContatosRoutingModule],
+  declarations: [
+    ContatosComponent,
+    DetalhesContatoComponent,
+    NovoContatoComponent,
+  ],
+  imports: [CommonModule, ContatosRoutingModule, ReactiveFormsModule],
 })
 export class ContatosModule {}
--- src/app/area-logada/contatos/novo-contato/novo-contato.component.ts
+++ src/app/area-logada/contatos/novo-contato/novo-contato.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-novo-contato',
   templateUrl: './novo-contato.component.html',
-  styleUrls: ['./novo-contato.component.scss']
+  styleUrls: ['./novo-contato.component.scss'],
 })
 export class NovoContatoComponent implements OnInit {
+  contatoForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    // this.contatoForm = new FormGroup({
+    //   nome: new FormControl(),
+    //   banco: new FormControl(),
+    // });
+
+    this.contatoForm = this.formBuilder.group({
+      nome: ['aaa'],
+      banco: [''],
+    });
   }
 
+  saveContact() {
+    console.log(this.contatoForm);
+  }
 }
